096be28b75a3d3adf6ca18d5a2858b1746342824
fefifofum <jorsellis@gmail.com>
2014-07-11 03:15:12 +0200

    Add Widevine blobs

--- rk3188-common/rk3188-vendor-blobs.mk
+++ rk3188-common/rk3188-vendor-blobs.mk
@@ -59,6 +59,15 @@ PRODUCT_COPY_FILES += \
     vendor/bq/rk3188-common/proprietary/lib/hw/gralloc.rk30board.so:system/lib/hw/gralloc.rk30board.so \
     vendor/bq/rk3188-common/proprietary/lib/hw/hwcomposer.rk30board.so:system/lib/hw/hwcomposer.rk30board.so
 
+# Widevine
+PRODUCT_COPY_FILES += \
+    vendor/bq/rk3188-common/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+    vendor/bq/rk3188-common/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
+    vendor/bq/rk3188-common/proprietary/vendor/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
+    vendor/bq/rk3188-common/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
+    vendor/bq/rk3188-common/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+    vendor/bq/rk3188-common/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so
+
 # Firmware
 PRODUCT_COPY_FILES += \
     vendor/bq/rk3188-common/proprietary/etc/firmware/BCM4330B1.hcd:system/etc/firmware/BCM4330B1.hcd
